2e0964d15de772d8958902587170c8a8a8b07243
indivp21 <87029524+indivp21@users.noreply.github.com>
2021-11-12 16:36:35 -0500

    20211112

--- src/components/gerencia.vue
+++ src/components/gerencia.vue
@@ -2,13 +2,6 @@
 <div id="mainger">
   <h3>Gerencias</h3>
   <nav3>
-     <h2>{{indicadorc}}</h2>
-     <label for="fgerencia"> {{gerenciac}} </label>
-     <form v-on:submit.prevent="ShowDetail">
-     <button v-on:click="gotoc"><font face="Century Gothic">--</font></button>
-     </form>
-  </nav3>
-  <nav3>
      <h2>{{indicadora}}</h2>
      <label for="fgerencia"> {{gerenciaa}} </label>
      <form v-on:submit.prevent="ShowDetail">
