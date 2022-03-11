82f999b03747e76f96150ef26a6892cae8cd8e31
indivp21 <87029524+indivp21@users.noreply.github.com>
2022-03-11 15:37:45 -0500

    20220311

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div class="header">
-      <h1>Indicadores Corte<br>25/02/2022 al 03/03/2022</h1>
+      <h1>Indicadores Corte<br>04/03/2022 al 10/03/2022</h1>
     </div>
     <div class="main-component">
       <router-view></router-view>
