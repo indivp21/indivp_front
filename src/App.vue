af983df42742e9d40ca3e572b986e4a9d5fe8a2d
aandresmon29 <71345919+aandresmon29@users.noreply.github.com>
2022-06-10 18:41:21 -0500

    update 20220610

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div class="header">
-      <h1>Indicadores Corte<br>20/05/2072 al 02/06/2022</h1>
+      <h1>Indicadores Corte<br>03/06/2072 al 09/06/2022</h1>
     </div>
     <div class="main-component">
       <router-view></router-view>
