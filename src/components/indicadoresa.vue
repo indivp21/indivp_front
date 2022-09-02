10096dcd63264f2587276332786fbaac1ac842c8
indivp21 <87029524+indivp21@users.noreply.github.com>
2022-09-02 17:16:40 -0500

    220902

--- src/components/indicadoresa.vue
+++ src/components/indicadoresa.vue
@@ -20,7 +20,7 @@
     <button v-on:click="gotoa"><font face="Century Gothic">--</font></button>
     </form>
   </nav2>
-  <nav2>
+  <nav8>
     <label1 for="fgerencia"> {{nombreinb}}  </label1>
     <label2 for="fgerencia"> </label2>
     <h4>{{porcentajeb}}</h4>
@@ -30,7 +30,7 @@
     <form v-on:submit.prevent="ShowDetail">
     <button v-on:click="gotob"><font face="Century Gothic">--</font></button>
     </form>
-  </nav2>
+  </nav8>
   <nav3>
     <label1 for="fgerencia"> {{nombreinc}}  </label1>
     <label2 for="fgerencia"> </label2>
@@ -53,7 +53,7 @@
     <button v-on:click="gotod"><font face="Century Gothic">--</font></button>
     </form>
   </nav3>
-  <nav4>
+  <nav11>
     <label1 for="fgerencia"> {{nombreine}}  </label1>
     <label2 for="fgerencia"> </label2>
     <h4>{{porcentajee}}</h4>
@@ -63,7 +63,7 @@
     <form v-on:submit.prevent="ShowDetail">
     <button v-on:click="gotoe"><font face="Century Gothic">--</font></button>
     </form>
-  </nav4>
+  </nav11>
   <nav4>
     <label1 for="fgerencia"> {{nombreinf}}  </label1>
     <label2 for="fgerencia"> </label2>
@@ -108,7 +108,7 @@
     <button v-on:click="gotoi"><font face="Century Gothic">--</font></button>
     </form>
   </nav4>
-  <nav12>
+  <nav4>
     <label1 for="fgerencia"> {{nombreinj}}  </label1>
     <label2 for="fgerencia"> </label2>
     <h4>{{porcentajej}}</h4>
@@ -118,7 +118,7 @@
     <form v-on:submit.prevent="ShowDetail">
     <button v-on:click="gotoj"><font face="Century Gothic">--</font></button>
     </form>
-  </nav12>
+  </nav4>
   <nav12>
     <label1 for="fgerencia"> {{nombreink}}  </label1>
     <label2 for="fgerencia"> </label2>
